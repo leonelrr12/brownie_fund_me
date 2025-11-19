cad0e376188e58c67a891841e52ad84b783515b3
Leonel <leonellrodriguez@gmail.com>
2025-11-19 15:25:17 -0500

    Dployed en network Sepolia

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // Evitar problemas de overflows en versiones de solidy 6 o menor.
 //import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink,sol";
 
-contract FundMe{
-
+contract FundMe {
     // Evitar problemas de overflows en versiones de solidy 6 o menor.
     //using SafeMathChainlink for uint256;
 
-    mapping ( address => uint256 ) public addressToAmountFunded;
+    mapping(address => uint256) public addressToAmountFunded;
 
     address[] public funders;
 
@@ -22,31 +21,40 @@ contract FundMe{
 
     function fund() public payable {
         // Minimo $50
-        uint256 minimumUsd = 50 * 10 ** 18; 
-        require(getConversionRate(msg.value) >= minimumUsd, "You need to spend more ETH!");     
+        uint256 minimumUsd = 50 * 10 ** 18;
+        require(
+            getConversionRate(msg.value) >= minimumUsd,
+            "You need to spend more ETH!"
+        );
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
-    function getVersion() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
         return priceFeed.version();
     }
 
     function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        ( , int price, , ,) = priceFeed.latestRoundData();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int price, , , ) = priceFeed.latestRoundData();
         return uint256(price * 10000000000);
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns ( uint256) {
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 100000000000000;
         return ethAmountInUsd;
     }
 
-    modifier onlyOwner{
-        require( msg.sender == owner );
+    modifier onlyOwner() {
+        require(msg.sender == owner);
         _;
     }
 
@@ -54,15 +62,14 @@ contract FundMe{
         return address(this).balance;
     }
 
-    function withdraw() payable onlyOwner public {
+    function withdraw() public payable onlyOwner {
         // only owner
         payable(msg.sender).transfer(address(this).balance);
-        for(uint256 funderIdx; funderIdx < funders.length; funderIdx++){
+        for (uint256 funderIdx; funderIdx < funders.length; funderIdx++) {
             address funder = funders[funderIdx];
             addressToAmountFunded[funder] = 0;
         }
 
         funders = new address[](0);
     }
-
 }
